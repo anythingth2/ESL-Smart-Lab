ceeaf9f9e948a9d0ffe3e1102accdf45947c1d88
ChiChaChai <chatchaishaetan@gmail.com>
2017-03-15 22:42:58 +0700

    add django authentication

--- mqtt.py
+++ mqtt.py
@@ -5,12 +5,12 @@ import thread
 input_data=""
 client=mqtt.Client()
 def on_connect(client,userdata,flags,rc):
-    print "Connected"
-    client.subscribe("test",2)
-
+    print "#Connected"
+    client.subscribe("/ESP/LED",2)
 def on_message(client,userdata,msg):
     print(str(msg.payload)+"")
-
+def on_disconnect(client,userdata,msg):
+    print "#Disconnected"
 def request_input():
     while True:
         input_data=raw_input()
@@ -18,9 +18,11 @@ def request_input():
 def hive_mqtt():
     # client=mqtt.Client()
     print "Hive_mqtt is Connecting...."
+    client.username_pw_set("NodeTest","1234")
     client.on_connect=on_connect
     client.on_message=on_message
-    client.connect("broker.mqttdashboard.com",1883,60)
+    client.on_disconnect=on_disconnect
+    client.connect("m11.cloudmqtt.com",12980,60)
     client.subscribe("test",2)
     client.loop_forever()
 
@@ -38,8 +40,4 @@ while True :
     elif input_data=="RECONNECT":
         client.reconnect()
     else:
-        client.publish("test",input_data,2)
-    
-
-
-
+        client.publish("/ESP/LED",input_data,2)
